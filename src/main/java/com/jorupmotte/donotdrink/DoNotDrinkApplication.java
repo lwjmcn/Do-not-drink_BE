9ef9382092e6d7e4b8c2fe0695c9dae90d2fe46f
lwjmcn <clarecse02@gmail.com>
2025-02-12 11:04:38 +0900

    feat: springboot init

---
+++ src/main/java/com/jorupmotte/donotdrink/DoNotDrinkApplication.java
@@ -0,0 +1,13 @@
+package com.jorupmotte.donotdrink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoNotDrinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoNotDrinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorupmotte/donotdrink/DoNotDrinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorupmotte.donotdrink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoNotDrinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
