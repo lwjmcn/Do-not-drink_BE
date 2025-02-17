fc1ffb9f65556441900e8db25ca111c9359c805c
lwjmcn <clarecse02@gmail.com>
2025-02-17 20:07:51 +0900

    feat: env, db setting

--- src/main/java/com/jorupmotte/donotdrink/DoNotDrinkApplication.java
+++ src/main/java/com/jorupmotte/donotdrink/DoNotDrinkApplication.java
@@ -1,12 +1,18 @@
 package com.jorupmotte.donotdrink;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DoNotDrinkApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach((entry -> System.setProperty(entry.getKey(), entry.getValue())));
+
 		SpringApplication.run(DoNotDrinkApplication.class, args);
 	}
 
