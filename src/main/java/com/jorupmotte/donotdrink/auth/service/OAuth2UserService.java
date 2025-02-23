0fa9e614fa44e81d11de15887347fdef812d83b9
lwjmcn <clarecse02@gmail.com>
2025-02-23 16:16:04 +0900

    fix: null point exception

--- src/main/java/com/jorupmotte/donotdrink/auth/service/OAuth2UserService.java
+++ src/main/java/com/jorupmotte/donotdrink/auth/service/OAuth2UserService.java
@@ -2,17 +2,12 @@ package com.jorupmotte.donotdrink.auth.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jorupmotte.donotdrink.auth.model.CustomOAuth2User;
-import com.jorupmotte.donotdrink.auth.repository.SocialLoginRepository;
-import com.jorupmotte.donotdrink.auth.repository.ThemeRepository;
-import com.jorupmotte.donotdrink.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,18 +17,19 @@ public class OAuth2UserService extends DefaultOAuth2UserService{
         OAuth2User oAuth2User =  super.loadUser(request);
         String oAuthClientName = request.getClientRegistration().getClientName();
 
+        String tokenId = null;
         try {
             // 터미널에 정보 출력
             System.out.println(new ObjectMapper().writeValueAsString(oAuth2User.getAttributes()));
+
+            if (oAuthClientName.equals("kakao")){ // baseUri/api/v1/auth/oauth2/kakao에서 테스트
+                tokenId = oAuth2User.getAttribute("id").toString();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        String tokenId = null;
 
-        if (oAuthClientName.equals("kakao")){ // baseUri/api/v1/auth/oauth2/kakao에서 테스트
-            tokenId = oAuth2User.getAttribute("id").toString();
-        }
 
         return new CustomOAuth2User(tokenId);
     }
