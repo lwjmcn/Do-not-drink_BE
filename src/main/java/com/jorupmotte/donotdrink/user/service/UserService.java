6b739c18357ea3b97cc2b45f75de2173c8c4df1f
lwjmcn <clarecse02@gmail.com>
2025-03-18 11:11:19 +0900

    feat: get user from security context

--- src/main/java/com/jorupmotte/donotdrink/user/service/UserService.java
+++ src/main/java/com/jorupmotte/donotdrink/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.jorupmotte.donotdrink.user.service;
 
 import com.jorupmotte.donotdrink.common.dto.response.ResponseDto;
+import com.jorupmotte.donotdrink.user.dto.response.UserFriendResponseDto;
 import com.jorupmotte.donotdrink.user.dto.response.UserMeResponseDto;
 import com.jorupmotte.donotdrink.user.model.User;
 import com.jorupmotte.donotdrink.user.repository.UserRepository;
@@ -12,23 +13,37 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+
 @Service
 @RequiredArgsConstructor
 public class UserService implements IUserService{
     private final UserRepository userRepository;
 
-    @Override
-    public ResponseEntity<? super UserMeResponseDto> getCurrentUser() {
-        // Security Context에서 인증 정보 꺼내기
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null) {
-            return ResponseDto.authorizationFail();
-        }
+    public User getUserFromSecurityContext(){
+        User user = null;
 
-        String userAccountId = authentication.getPrincipal().toString();
+        try {
+            // Security Context에서 인증 정보 꺼내기
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null) {
+                return null;
+            }
+            String userAccountId = authentication.getPrincipal().toString();
 
         // 유저 정보 가져오기
         Optional<User> userOptional = userRepository.findByAccountId(userAccountId);
+            // db 조회
+            Optional<User> userOptional = userRepository.findByAccountId(userAccountId);
+            if (userOptional.isEmpty()) {
+                return null;
+            }
+            user = userOptional.get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
         if(userOptional.isEmpty()){
             return ResponseDto.databaseError();
         }
