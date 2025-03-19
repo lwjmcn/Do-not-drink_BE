c53af08da700b5f91a6550c8d86d8411d827478b
lwjmcn <clarecse02@gmail.com>
2025-03-20 01:20:44 +0900

    fix: update query in repository

--- src/main/java/com/jorupmotte/donotdrink/friend/repository/FriendRequestRepository.java
+++ src/main/java/com/jorupmotte/donotdrink/friend/repository/FriendRequestRepository.java
@@ -2,7 +2,10 @@ package com.jorupmotte.donotdrink.friend.repository;
 
 import com.jorupmotte.donotdrink.common.type.FriendStatusType;
 import com.jorupmotte.donotdrink.friend.model.FriendRequest;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +17,9 @@ public interface FriendRequestRepository extends JpaRepository<FriendRequest, Lo
     List<FriendRequest> findAllByTo_Id(Long receiverId);
 
     boolean existsByFrom_IdAndTo_Id(Long fromId, Long toId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE FriendRequest fr SET fr.status = :status WHERE fr.id = :requestId")
     void updateStatusById(Long requestId, FriendStatusType status);
 }
