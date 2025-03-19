481259df6d04594e73e292c12fe7a5a4650e929c
lwjmcn <clarecse02@gmail.com>
2025-03-20 00:58:59 +0900

    feat: sse subscribe api

--- src/main/java/com/jorupmotte/donotdrink/budget/controller/BudgetFriendController.java
+++ src/main/java/com/jorupmotte/donotdrink/budget/controller/BudgetFriendController.java
@@ -3,21 +3,45 @@ package com.jorupmotte.donotdrink.budget.controller;
 import com.jorupmotte.donotdrink.budget.dto.request.ReactToRequestDto;
 import com.jorupmotte.donotdrink.budget.dto.response.ReactToResponseDto;
 import com.jorupmotte.donotdrink.budget.service.ReactionService;
+import com.jorupmotte.donotdrink.friend.service.FriendService;
+import com.jorupmotte.donotdrink.friend.service.SseEmitterService;
+import com.jorupmotte.donotdrink.user.model.User;
+import com.jorupmotte.donotdrink.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/users/{receiverId}/budgets/current/reactions")
+@RequestMapping("api/v1/users/{friendId}/budgets/current/reactions")
 public class BudgetFriendController {
+    private final UserService userService;
     private final ReactionService reactionService;
+    private final SseEmitterService sseEmitterService;
+    private final FriendService friendService;
+
+    @GetMapping("/current/reactions")
+    public SseEmitter subscribeReaction(
+            @PathVariable Long friendId
+    ) {
+        User userMe = userService.getUserFromSecurityContext();
+        if(userMe == null) {
+            System.out.println("reaction subscribe: authorization fail");
+            return null;
+        }
+        if(friendService.isFriend(userMe.getId(), friendId)){
+            System.out.println("reaction subscribe: not a friend");
+            return null;
+        }
+        return sseEmitterService.reactionSubscribe(friendId);
+    }
 
     @PostMapping("/current/reactions")
     public ResponseEntity<? super ReactToResponseDto> reactTo(
-            @PathVariable Long receiverId,
+            @PathVariable Long friendId, // receiverId
             @RequestBody @Valid ReactToRequestDto requestBody) {
-        return reactionService.reactTo(receiverId, requestBody);
+        return reactionService.reactTo(friendId, requestBody);
     }
 }
--- src/main/java/com/jorupmotte/donotdrink/budget/controller/BudgetMeController.java
+++ src/main/java/com/jorupmotte/donotdrink/budget/controller/BudgetMeController.java
@@ -6,17 +6,22 @@ import com.jorupmotte.donotdrink.budget.dto.response.BudgetSetResponseDto;
 import com.jorupmotte.donotdrink.budget.dto.response.ReactionCurrentResponseDto;
 import com.jorupmotte.donotdrink.budget.service.BudgetService;
 import com.jorupmotte.donotdrink.budget.service.ReactionService;
+import com.jorupmotte.donotdrink.friend.service.SseEmitterService;
+import com.jorupmotte.donotdrink.user.model.User;
+import com.jorupmotte.donotdrink.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/users/me/budgets")
 public class BudgetMeController {
     private final BudgetService budgetService;
-    private final ReactionService reactionService;
+    private final SseEmitterService sseEmitterService;
+    private final UserService userService;
 
     @GetMapping("/current/remains")
     public ResponseEntity<? super BudgetRemainingResponseDto> getRemainingBudget() {
@@ -31,7 +36,16 @@ public class BudgetMeController {
     }
 
     @GetMapping(value = "/current/reactions")
-    public ResponseEntity<? super ReactionCurrentResponseDto> getMyCurrentReactions() {
-        return reactionService.getMyCurrentReactionsFromDb();
+    public SseEmitter subscribeMyReaction() {
+        User userMe = userService.getUserFromSecurityContext();
+        if(userMe == null){
+            System.out.println("reaction subscribe: authorization fail");
+            return null;
+        }
+
+        return sseEmitterService.reactionSubscribe(userMe.getId());
     }
+//    public ResponseEntity<? super ReactionCurrentResponseDto> getMyCurrentReactions() {
+//        return reactionService.getMyCurrentReactionsFromDb();
+//    }
 }
