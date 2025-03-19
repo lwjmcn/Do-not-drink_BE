89e491822bd8b9411b0bbc0e5492ad2cfc4d7e25
lwjmcn <clarecse02@gmail.com>
2025-03-20 01:23:58 +0900

    fix: sum query in repository

--- src/main/java/com/jorupmotte/donotdrink/budget/repository/TransactionRepository.java
+++ src/main/java/com/jorupmotte/donotdrink/budget/repository/TransactionRepository.java
@@ -1,13 +1,17 @@
 package com.jorupmotte.donotdrink.budget.repository;
 
 import com.jorupmotte.donotdrink.budget.model.Transaction;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    @Transactional
+    @Query("SELECT SUM(t.amount) FROM Transaction t WHERE t.user.id = ?1 AND t.date >= ?2 AND t.date <= ?3")
     Long sumAllByUser_IdAndDateGreaterThanEqualAndDateLessThanEqual(Long userId, LocalDateTime startDate, LocalDateTime endDate);
 
 }
